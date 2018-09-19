62db8660970eaec8eee55856ef699b2fd9d600ea
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2018-09-19 11:56:49 -0500

    Attempting to force whitespace

--- src/components/Chirp.jsx
+++ src/components/Chirp.jsx
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
+const forceWhitespace = { whiteSpace: 'pre-wrap' };
 
 class Chirp extends Component {
-    constructor(props) {
-        super(props);
-    }
 
 
 
     render() {
         return (
-            <div className='Chirp'>
-                <div className='UserInfo'>
-                    <p>This is a test Chirp!</p>
-                </div>
-            </div>
+            <p style={forceWhitespace}>This is a test Chirp, please ignore!!</p>
         );
     }
 }
